ee3e767056abc7c927513f770fedf40edc607a5f
James Brundage <@github.com>
2024-04-05 19:08:10 -0700

    feat: CreationTime TimeZone fixes ( Fixes #24, Fixes #28, Fixes #40 )

--- Types/docker.images/get_CreationTime.ps1
+++ Types/docker.images/get_CreationTime.ps1
@@ -1,6 +1,6 @@
 $this.CreatedAt -replace 
     '(?<=\d)\s(?=\d)', 'T' -replace
     '\s', ':' -replace 
-    ':-','-' -replace 
+    ':([\-\+])','$1' -replace 
     ':\w{3}$' -replace 
     '\d{2}$', ':$0' -as [DateTime]
