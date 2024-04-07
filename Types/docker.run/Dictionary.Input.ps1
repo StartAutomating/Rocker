9ec2b68551e8a0f9b9d841576011be433b6ad349
James Brundage <@github.com>
2024-04-07 10:02:36 -0700

    fix: docker.run.Dictionary.Input --publish support ( Fixes #57 )

--- Types/docker.run/Dictionary.Input.ps1
+++ Types/docker.run/Dictionary.Input.ps1
@@ -1,5 +1,15 @@
+<#
+.SYNOPSIS
+    Gets a dictionary as `docker run` input.
+.DESCRIPTION
+    Converts a dictionary to a list of arguments to `docker run`.
 
+    If the dictionary contains pairs of integers, they are treated as port mappings.
+    If the dictionary contains paths, they are treated as volumes.
+    Otherwise, they are treated as environment variables.    
+#>
 param(
+# The dictionary
 [Parameter(Mandatory)]
 [Collections.IDictionary]
 $Dictionary
@@ -7,8 +17,13 @@ $Dictionary
 
 if (-not $Dictionary.Count) { return }
 foreach ($keyValuePair in $Dictionary.GetEnumerator()) {
+    # If it's a pair of digits, treat it as --publish
+    if ($keyValuePair.Key -is [int] -and $keyValuePair.Value -is [int]) {
+        "--publish"
+        "$($keyValuePair.Key):$($keyValuePair.Value)"
+    }
     # If it's a path, treat it as a --volume
-    if ($keyValuePair.Key -match '[\\/]')
+    elseif ($keyValuePair.Key -match '[\\/]')
     {
         "--volume"
         "$($keyValuePair.Key):$($keyValuePair.Value)"
