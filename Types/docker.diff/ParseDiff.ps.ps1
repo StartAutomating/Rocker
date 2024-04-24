b46c8b9ffa018a3c8dfa050e669c44f8fe893e90
James Brundage <@github.com>
2024-04-24 14:49:33 -0700

    feat: docker diff parser ( Fixes #70 )

---
+++ Types/docker.diff/ParseDiff.ps.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Parses the output of `docker diff`.
+.DESCRIPTION
+    Parses the output of `docker diff` and returns a custom object with the following properties:
+    
+    - CommandLine: The command line that produced the output.
+    - ContainerID: The ID of the container being diffed.
+    - ChangeType: The type of change (Added, Changed, or Deleted).
+    - Path: The path of the changed file.
+#>
+[ValidatePattern("^docker\s{1,}diff\s{1,}")]
+param(
+# The content being parsed (this will be piped in line-by-line).
+[vfp()]
+[string]
+$Content,
+
+# The command line that produced the content.
+[string]
+$CommandLine
+)
+
+begin {
+    $ContainerID = @($CommandLine) -match '\s{1,}(?<id>[0-9][a-f]+)\s{1,}' -replace '\s'
+}
+
+process {
+    if ($content -cnotmatch '^[ACD]\s') { return $content }
+    [PSCustomObject]@{
+        PSTypeName = 'docker.diff'
+        CommandLine = $CommandLine
+        ContainerID = $ContainerID
+        ChangeType = switch ($content[0]) {
+            'A' { 'Added' }
+            'C' { 'Changed' }
+            'D' { 'Deleted' }
+        }
+        Path = $content -replace '^[ACD]\s{1,}'
+    }
+}
